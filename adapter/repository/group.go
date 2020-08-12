ef89aebc31c1275d7c95ccc4eaedfd1514f8f160
burkovski <burkovski.danil@icloud.com>
2020-08-12 15:57:04 +0300

    GetByID in GroupRepository

--- adapter/repository/group.go
+++ adapter/repository/group.go
@@ -2,10 +2,10 @@ package repository
 
 import (
 	dbx "github.com/go-ozzo/ozzo-dbx"
-	"github.com/rs/zerolog/log"
 
 	"gitlab.jooble.com/marketing_tech/yandex_bidder/domain"
 	"gitlab.jooble.com/marketing_tech/yandex_bidder/infrastructure/store/sql"
+	"gitlab.jooble.com/marketing_tech/yandex_bidder/usecase"
 )
 
 type (
@@ -20,15 +20,46 @@ func NewGroupRepository(s *sql.Store) usecase.GroupRepo {
 	}
 }
 
-func (r *groupRepo) GetAll() []*domain.Group {
+func (r *groupRepo) GetAll() ([]*domain.Group, error) {
 	var groups []*domain.Group
 	query := r.store.DB.
 		Select("id, name, schedule_start, schedule_interval, strategy").
 		From("groups")
 
 	if err := query.All(groups); err != nil {
-		log.Error().Err(err)
+		return nil, err
 	}
 
-	return groups
+	return groups, nil
 }
+
+func (r *groupRepo) GetByID(id int) (*domain.Group, error) {
+	group := new(domain.Group)
+	query := r.store.DB.NewQuery(
+		`SELECT id, name, schedule_start, schedule_interval, strategy
+ 		 FROM groups
+		 WHERE id = {:id}`,
+	).Bind(dbx.Params{"id": id})
+
+	if err := query.One(group); err != nil {
+
+	}
+}
+
+// func (r *groupRepo) Update(group *domain.GroupUpdateIn) error {
+// params := dbx.Params{
+// 	"schedule_start":    group.Start,
+// 	"schedule_interval": group.Interval,
+// 	"strategy":          group.Strategy,
+// }
+// where := dbx.NewExp("id = {:id}", dbx.Params{"id": group.ID})
+
+// query := r.store.DB.Update("groups", params, where)
+// if _, err := query.Execute(); err != nil {
+// 	return err
+// }
+
+// return r.store.DB.Model(group).Exclude("name").Update()
+
+// return nil
+// }
