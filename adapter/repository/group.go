0f94a3a5bcd27a7603142319791599707c974632
burkovski <burkovski.danil@icloud.com>
2020-08-12 16:09:34 +0300

    Fix GetByID in GroupRepository

--- adapter/repository/group.go
+++ adapter/repository/group.go
@@ -1,28 +1,29 @@
 package repository
 
 import (
+	"database/sql"
+
 	dbx "github.com/go-ozzo/ozzo-dbx"
-	"github.com/rs/zerolog/log"
 
 	"gitlab.jooble.com/marketing_tech/yandex_bidder/domain"
-	"gitlab.jooble.com/marketing_tech/yandex_bidder/infrastructure/store/sql"
+	"gitlab.jooble.com/marketing_tech/yandex_bidder/usecase"
 )
 
 type (
 	groupRepo struct {
-		store *sql.Store
+		db dbx.Builder
 	}
 )
 
-func NewGroupRepository(s *sql.Store) usecase.GroupRepo {
+func NewGroupRepo(db dbx.Builder) usecase.GroupRepo {
 	return &groupRepo{
-		store: s,
+		db: db,
 	}
 }
 
 func (r *groupRepo) GetAll() ([]*domain.Group, error) {
 	groups := make([]*domain.Group, 0)
-	rows, err := r.store.DB.
+	rows, err := r.db.
 		Select("id", "name", "schedule_start", "schedule_interval", "strategy").
 		From("groups").
 		Rows()
@@ -43,31 +44,34 @@ func (r *groupRepo) GetAll() ([]*domain.Group, error) {
 
 func (r *groupRepo) GetByID(id int) (*domain.Group, error) {
 	group := new(domain.Group)
-	query := r.store.DB.NewQuery(
-		`SELECT id, name, schedule_start, schedule_interval, strategy
- 		 FROM groups
-		 WHERE id = {:id}`,
-	).Bind(dbx.Params{"id": id})
+	query := r.db.Select("id", "name", "schedule_start", "schedule_interval", "strategy").
+		From("groups").
+		Where(dbx.NewExp("id", dbx.Params{"id": id}))
 
 	if err := query.One(group); err != nil {
-
+		if err == sql.ErrNoRows {
+			err = domain.ErrGroupNotFound
+		}
+		return nil, err
 	}
+
+	return group, nil
 }
 
-// func (r *groupRepo) Update(group *domain.GroupUpdateIn) error {
-// params := dbx.Params{
-// 	"schedule_start":    group.Start,
-// 	"schedule_interval": group.Interval,
-// 	"strategy":          group.Strategy,
-// }
-// where := dbx.NewExp("id = {:id}", dbx.Params{"id": group.ID})
+func (r *groupRepo) Update(group *domain.GroupUpdateIn) error {
+	// params := dbx.Params{
+	// 	"schedule_start":    group.Start,
+	// 	"schedule_interval": group.Interval,
+	// 	"strategy":          group.Strategy,
+	// }
+	// where := dbx.NewExp("id = {:id}", dbx.Params{"id": group.ID})
+	//
+	// query := r.store.DB.Update("groups", params, where)
+	// if _, err := query.Execute(); err != nil {
+	// 	return err
+	// }
 
-// query := r.store.DB.Update("groups", params, where)
-// if _, err := query.Execute(); err != nil {
-// 	return err
-// }
+	// return r.store.DB.Model(group).Exclude("name").Update()
 
-// return r.store.DB.Model(group).Exclude("name").Update()
-
-// return nil
-// }
+	return nil
+}
